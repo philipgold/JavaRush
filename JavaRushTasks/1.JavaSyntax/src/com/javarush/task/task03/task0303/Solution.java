00f737753e42d76fa0cd8cd81eafa0767d8851dc
Philip Goldman <philip.goldm@gmail.com>
2017-07-09 20:57:30 +0300

    Updated task0303

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
@@ -7,9 +7,13 @@ package com.javarush.task.task03.task0303;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.println(convertEurToUsd(50, 3.5));
+        System.out.println(convertEurToUsd(10,2.5));
+
     }
 
     public static double convertEurToUsd(int eur, double course) {
         //напишите тут ваш код
+        return eur * course;
     }
 }
