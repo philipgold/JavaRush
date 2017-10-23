312dc1c5566eeb4906a2efda0e9f4a03da1f10cb
Philip Goldman <philip.goldm@gmail.com>
2017-10-23 16:34:10 +0300

    Completed task0531

--- 1.JavaSyntax/src/com/javarush/task/task05/task0531/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0531/Solution.java
@@ -5,6 +5,17 @@ import java.io.InputStreamReader;
 
 /* 
 Совершенствуем функциональность
+Текущая реализация: Программа вводит два числа с клавиатуры и выводит минимальное из них на экран.
+Новая задача: Программа вводит пять чисел с клавиатуры и выводит минимальное из них на экран.
+
+
+Требования:
+1. Программа должна считывать числа с клавиатуры.
+2. В классе должен быть метод public static void main.
+3. В классе должен быть метод public static min, принимающий 5 параметров типа int.
+4. Метод min должен возвращать минимум из 5 переданных чисел.
+5. Программа должна выводить строку, которая начинается на "Minimum = ".
+6. Программа должна выводить строку, которая заканчивается минимальным из 5 введенных чисел.
 */
 
 public class Solution {
@@ -13,14 +24,24 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
 
-        int minimum = min(a, b);
+
+        int minimum = min(a, b, c, d, e);
 
         System.out.println("Minimum = " + minimum);
     }
 
 
-    public static int min(int a, int b) {
-        return a < b ? a : b;
+    public static int min(int a, int b, int c, int d, int e) {
+        int[] array = {a, b, c, d, e};
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]< min)
+                min = array[i];
+        }
+        return min;
     }
 }
