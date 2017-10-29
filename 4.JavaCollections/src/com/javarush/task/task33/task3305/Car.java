2a981bb9c89240ed748fefb64747861d507e3363
Philip Goldman <philip.goldm@gmail.com>
2017-10-29 14:29:37 +0200

    Completed task3305

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3305/Car.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task33.task3305;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+
+public class Car extends Auto {
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3305/Motorbike.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task33.task3305;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property="className")
+public class Motorbike extends Auto {
+    private String owner;
+
+    public Motorbike(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Motorbike{" +
+                "name='" + name + '\'' +
+                ", owner='" + owner + '\'' +
+                '}';
+    }
+}
